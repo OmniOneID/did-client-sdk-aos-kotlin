fbf0d9c48bceba755b3e5e702f231897ea95720d
sjkim <sjkim@raoncorp.com>
2025-03-24 15:17:17 +0900

    Initial commit

--- source/did-client-sdk-aos/did-wallet-sdk-aos/build.gradle.kts
+++ source/did-client-sdk-aos/did-wallet-sdk-aos/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.core:core-ktx:1.12.0")
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.5.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
